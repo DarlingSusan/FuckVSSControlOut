4480357c3689c6dc99870d0800af9794db75d019
UncleSusan <mcz555@126.com>
2016-01-07 09:01:55 +0800

    changes log

---
+++ FuckVSSControlOut/FuckVSSControlOut/FrmFuckVSSOut.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace FuckVSSControlOut
+{
+    partial class FrmFuckVSSOut
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmFuckVSSOut));
+            this.btnStart = new System.Windows.Forms.Button();
+            this.tbxDirectory = new System.Windows.Forms.TextBox();
+            this.btnChooseDirect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStart.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnStart.Location = new System.Drawing.Point(12, 87);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(320, 32);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "开始移除VSS控制代码";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // tbxDirectory
+            // 
+            this.tbxDirectory.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbxDirectory.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.tbxDirectory.Location = new System.Drawing.Point(12, 52);
+            this.tbxDirectory.Name = "tbxDirectory";
+            this.tbxDirectory.Size = new System.Drawing.Size(320, 29);
+            this.tbxDirectory.TabIndex = 1;
+            // 
+            // btnChooseDirect
+            // 
+            this.btnChooseDirect.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnChooseDirect.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnChooseDirect.Location = new System.Drawing.Point(12, 12);
+            this.btnChooseDirect.Name = "btnChooseDirect";
+            this.btnChooseDirect.Size = new System.Drawing.Size(320, 34);
+            this.btnChooseDirect.TabIndex = 2;
+            this.btnChooseDirect.Text = "选择目录";
+            this.btnChooseDirect.UseVisualStyleBackColor = true;
+            this.btnChooseDirect.Click += new System.EventHandler(this.btnChooseDirect_Click);
+            // 
+            // FrmFuckVSSOut
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(347, 137);
+            this.Controls.Add(this.btnChooseDirect);
+            this.Controls.Add(this.tbxDirectory);
+            this.Controls.Add(this.btnStart);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmFuckVSSOut";
+            this.Text = "清除VSS控制小工具";
+            this.Activated += new System.EventHandler(this.FrmFuckVSSOut_Activated);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox tbxDirectory;
+        private System.Windows.Forms.Button btnChooseDirect;
+    }
+}
---
+++ FuckVSSControlOut/FuckVSSControlOut/FrmFuckVSSOut.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace FuckVSSControlOut
+{
+    public partial class FrmFuckVSSOut : Form
+    {
+        public FrmFuckVSSOut()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChooseDirect_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fd = new FolderBrowserDialog();
+            fd.Description = "";
+            fd.ShowNewFolderButton = true;
+            if (fd.ShowDialog() == DialogResult.OK)
+            {
+                tbxDirectory.Text = fd.SelectedPath;
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            string folder = tbxDirectory.Text;
+            if (string.IsNullOrEmpty(folder.Trim()))
+            {
+                MessageBox.Show("尚未选择任意文件夹!\n麻蛋快去选!~");
+                return;
+            }
+            if (MessageBox.Show("你当前选择的目录是:" + folder + ",\n是否确定清楚该目录下的VSS控制代码?", "", MessageBoxButtons.OKCancel) == DialogResult.OK)
+            {
+                //MessageBox.Show("OK!~");
+                DirectoryInfo di = new DirectoryInfo(folder);
+                List<FileInfo> vssList = di.GetFiles("*.scc", SearchOption.AllDirectories).ToList<FileInfo>();
+                vssList.AddRange(di.GetFiles("*.vssscc", SearchOption.AllDirectories).ToList());
+                vssList.AddRange(di.GetFiles("*.vspscc", SearchOption.AllDirectories).ToList());
+                foreach (FileInfo fi in vssList)
+                {
+                    fi.Attributes = FileAttributes.Normal;
+                    fi.Delete();
+                }
+                FileInfo[] sln = di.GetFiles("*.sln", SearchOption.AllDirectories);
+                for (int i = 0; i < sln.Length; i++)
+                {
+                    sln[i].Attributes = FileAttributes.Normal;
+                    string file = File.ReadAllText(sln[i].FullName);
+                    file = Regex.Replace(file, @"GlobalSection\(SourceCodeControl\).*?EndGlobalSection", "", RegexOptions.Singleline);
+                    StreamWriter sw = new StreamWriter(sln[i].FullName, false);
+                    sw.Write(file);
+                    sw.Close();
+                }
+
+                FileInfo[] csproj = di.GetFiles("*.csproj", SearchOption.AllDirectories);
+                for (int i = 0; i < csproj.Length; i++)
+                {
+                    csproj[i].Attributes = FileAttributes.Normal;
+                    string file = File.ReadAllText(csproj[i].FullName);
+                    file = Regex.Replace(file, @"<SccProjectName>.*?</SccProvider>", "", RegexOptions.Singleline);
+                    StreamWriter sw = new StreamWriter(csproj[i].FullName, false);
+                    sw.Write(file);
+                    sw.Close();
+                }
+                MessageBox.Show("VSS控制代码已清除完毕！~");
+            }
+        }
+
+        private void FrmFuckVSSOut_Activated(object sender, EventArgs e)
+        {
+            tbxDirectory.Focus();
+        }
+    }
+
+}
